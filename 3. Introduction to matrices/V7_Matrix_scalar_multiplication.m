ce01fe939b0fe65d38537f0a3f6ab76eda76c61d
kavinda-ravishan <kvnravishan@outlook.com>
2021-09-26 20:33:46 +0530

    V7_Matrix_scalar_multiplication

---
+++ 3. Introduction to matrices/V7_Matrix_scalar_multiplication.m
@@ -0,0 +1,14 @@
+%%
+%     COURSE: Linear algebra: theory and implementation
+%    SECTION: Introduction to matrices
+%      VIDEO: Matrix-scalar multiplication
+% Instructor: sincxpress.com
+%
+%%
+
+M = [1 2; 2 5];
+s = 2;
+
+% pre- and post-multiplication is the same:
+M*s
+s*M

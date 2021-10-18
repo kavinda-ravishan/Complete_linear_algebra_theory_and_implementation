998986622e22d128c6c12bdead851f6eb10effa5
kavinda-ravishan <kvnravishan@outlook.com>
2021-10-18 17:00:41 +0530

    V6_Order_of_operations_on_matrices

---
+++ 4. Matrix multiplications/V6_Order_of_operations_on_matrices.m
@@ -0,0 +1,23 @@
+%%
+%     COURSE: Linear algebra: theory and implementation
+%    SECTION: Matrix multiplication
+%      VIDEO: Order of operations on matrices
+% Instructor: sincxpress.com
+%
+%%
+clc, clear, close all;
+
+n = 5;
+L = randn(n);
+I = randn(n);
+V = randn(n);
+E = randn(n);
+
+% result of "forward" multiplication and then transpose
+res1 = (L*I*V*E)';
+
+% result of "flipped" multiplication of transposed matrices
+res2 = E'*V'*I'*L';
+
+% test equality by subtracting (ignore possible computer rounding errors)
+res1-res2
